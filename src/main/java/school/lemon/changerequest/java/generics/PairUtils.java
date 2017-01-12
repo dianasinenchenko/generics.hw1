9554041bf7a511f793cded9d8511e0982219d12f
dianasinenchenko <diana.sinenchenko@gmail.com>
2017-01-12 19:33:41 +0200

    PairUtils (equals and compareTo    getKey getValue)

--- src/main/java/school/lemon/changerequest/java/generics/PairUtils.java
+++ src/main/java/school/lemon/changerequest/java/generics/PairUtils.java
@@ -5,14 +5,31 @@ package school.lemon.changerequest.java.generics;
  */
 public class PairUtils {
     public static <K, V> boolean equals(Pair<K, V> firstPair, Pair<K, V> secondPair) {
-        if (firstPair == null || secondPair == null) {
-            return false;
-        }
         return firstPair.getKey().equals(secondPair.getKey()) && firstPair.getValue().equals(secondPair.getValue());
     }
 
     public static <K extends Comparable<K>, V> int compareTo(Pair<K, V> firstPair, Pair<K, V> secondPair) {
-        return firstPair.getKey().compareTo(secondPair.getKey());
+        try {
+            return firstPair.getKey().compareTo(secondPair.getKey());
+        } catch (NullPointerException e) {
+            return -1;
+        }
+    }
+
+    public static <K, V> V[] getValue(Pair<K, V>[] pairValue) {
+        V[] arrValue = (V[]) new Object[pairValue.length];
+        for (int i = 0; i < pairValue.length; i++) {
+            arrValue[i] = (V) pairValue[i].getValue();
+        }
+        return arrValue;
+    }
+
+    public static <K, V> K[] getKey(Pair<K, V>[] pairKey) {
+        K[] arrKey = (K[]) new Object[pairKey.length];
+        for (int i = 0; i < pairKey.length; i++) {
+            arrKey[i] = (K) pairKey[i].getValue();
+        }
+        return arrKey;
     }
 
 }
