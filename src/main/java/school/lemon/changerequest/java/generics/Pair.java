191f9b21a0abcdba1edc1fe8320eb18d0f5bfc5f
dianasinenchenko <diana.sinenchenko@gmail.com>
2017-01-12 17:49:53 +0200

    Pair

---
+++ src/main/java/school/lemon/changerequest/java/generics/Pair.java
@@ -0,0 +1,31 @@
+package school.lemon.changerequest.java.generics;
+
+/**
+ * Created by Diana on 12.01.2017.
+ */
+public class Pair<K, V> {
+    private K key;
+    private V value;
+
+    public Pair(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public void setKey(K key1) {
+        this.key = key;
+    }
+
+    public K getKey() {
+        return key;
+    }
+
+    public void setValue(V value) {
+        this.value = value;
+    }
+
+    public V getValue() {
+        return value;
+    }
+
+}
