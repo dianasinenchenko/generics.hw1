8d8aeb4eaa849cc3dc4edf892618a4f3ebc41b07
dianasinenchenko <diana.sinenchenko@gmail.com>
2017-01-12 18:09:26 +0200

    PairUtils (equals and compareTo)

--- src/main/java/school/lemon/changerequest/java/generics/PairUtils.java
+++ src/main/java/school/lemon/changerequest/java/generics/PairUtils.java
@@ -7,4 +7,8 @@ public class PairUtils {
     public static <K, V> boolean equals(Pair<K, V> firstPair, Pair<K, V> secondPair) {
         return firstPair.getKey().equals(secondPair.getKey()) && firstPair.getValue().equals(secondPair.getValue());
     }
+
+    public static <K extends Comparable<K>, V> int compareTo(Pair<K, V> firstPair, Pair<K, V> secondPair) {
+        return firstPair.getKey().compareTo(secondPair.getKey());
+    }
 }
